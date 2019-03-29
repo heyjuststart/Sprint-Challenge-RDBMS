b062c8e558e16194bbdc741834f937677df587bb
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-29 11:55:56 -0400

    add projects, actions seeds

--- data/seeds/001_projects.js
+++ data/seeds/001_projects.js
@@ -1,8 +1,8 @@
 
 exports.seed = function(knex, Promise) {
   return knex('projects').insert([
-    {name: 'project1' },
-    {name: 'project2' },
-    {name: 'project3' }
+    {name: 'project1', description: 'description1' },
+    {name: 'project2', description: 'description2' },
+    {name: 'project3', description: 'description3' }
   ]);
 };
